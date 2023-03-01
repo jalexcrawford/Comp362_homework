da2c0765153a872a616e588516ec939ec9c5f7ac
jalexcrawford <44788119+jalexcrawford@users.noreply.github.com>
2023-03-01 12:30:29 -0800

    update forking structure

--- CrawfordAlexLab03/proc_hier/proc_hier.c
+++ CrawfordAlexLab03/proc_hier/proc_hier.c
@@ -11,86 +11,103 @@
 int main() {
     pid_t pid;
 
-        // fork a child process
+    // fork a child process
     pid = fork();
 
     if (pid < 0) // error occurred
-        oops("Fork Failed!");
+    oops("Fork Failed!");
 
-    if (pid == 0) // child 1.1
+    if (pid == 0) // child
     {
-        pid_t pid_foo;
-        printf("I am the child %d\n", getpid());
-
-            // fork a child process
-        pid_foo = fork();
-
-        if (pid_foo < 0) // error occurred
+        // this is 1.1
+        pid = fork();
+        if(pid < 0)
+        {
             oops("Fork Failed!");
-
-            if (pid_foo == 0) // child 1.1.1
-            {
-                pid_t pid_bar;
-                printf("I am the child %d\n", getpid());
-            
-                // fork a child process
-                pid_bar = fork();    
-                if (pid_bar < 0) // error occurred
-                    oops("Fork Failed!");
-
-                if (pid_bar == 0) // child 1.1.2
-                {
-                    printf("I am the child %d\n", getpid());
-                    if (execlp("./iam", "iam", "Hello Parent! 1.1.2 Please do not kill me!", NULL) < 0) 
-                        oops("Execlp Failed!");
-                }
-                if (execlp("./iam", "iam", "Hello Parent! 1.1.1 Please do not kill me!", NULL) < 0) 
-                        oops("Execlp Failed!");
-                
-                }
-                        if (execlp("./iam", "iam", "Hello Parent! 1.1 Please do not kill me!", NULL) < 0) 
-                oops("Execlp Failed!");
         }
-        // fork a child process
-    pid = fork();
+        if(pid == 0)
+        {
+            //this is 1.1.1
+            if (execlp("./iam", "iam", "1.1.1", NULL) < 0)
+                oops("Fork Failed (1.1.1)");
+        }
+        else
+        {
+            //this is still 1.1
+            pid = fork();
+            if(pid < 0)
+            {
+                oops("Fork Failed!");
+            }
+            if(pid == 0)
+            {
+                //this is 1.1.2
+                if (execlp("./iam", "iam", "1.1.2", NULL) < 0) oops("Fork Failed (1.1.2)");
+            }
+            else
+            {
+                //this is still 1.1
+                if(execlp("./iam", "iam", "1.1", NULL) < 0)
+                    oops("Fork Failed (1.1)");
+            }
+        }
 
-    if (pid == 0) // child 1.2
+        printf("I am the child, pid = %d\n", getpid());
+        if (execlp("./iam", "iam", "Hello Parent! Please do not kill me!", NULL) < 0) 
+            oops("Execlp Failed!");
+
+    }
+    else
     {
-        pid_t pid_foo;
-        printf("I am the child %d\n", getpid());
-
-            // fork a child process
-        pid_foo = fork();
-
-        if (pid_foo < 0) // error occurred
-            oops("Fork Failed!");
-
-            if (pid_foo == 0) // child 1.2.1
-            {
-                pid_t pid_bar;
-                printf("I am the child %d\n", getpid());
-            
-                // fork a child process
-                pid_bar = fork();    
-                if (pid_bar < 0) // error occurred
-                    oops("Fork Failed!");
-
-                if (pid_bar == 0) // child 1.2.2
-                {
-                    printf("I am the child %d\n", getpid());
-                    if (execlp("./iam", "iam", "Hello Parent! 1.1.2 Please do not kill me!", NULL) < 0) 
-                        oops("Execlp Failed!");
-                }
-                if (execlp("./iam", "iam", "Hello Parent! 1.1.1 Please do not kill me!", NULL) < 0) 
-                        oops("Execlp Failed!");
-                
-                }
-                        if (execlp("./iam", "iam", "Hello Parent! 1.1 Please do not kill me!", NULL) < 0) 
-                oops("Execlp Failed!");
+        // This is 1
+        // create 1.2 and it's children
+        pid = fork();
+        if (pid < 0)
+        {
+            oops("fork failed");
         }
+        if(pid == 0)
+        {
+            //this is still 1.2
+            pid = fork();
+            if(pid < 0)
+            {
+                oops("Fork Failed!");
+            }
+            if(pid == 0){
+            //this is 1.2.1
+                if(execlp("./iam", "iam", "1.2.1", NULL) < 0)
+                    oops("ForkFailed (1.2.1");
+            }
+        }
+        
+    else{
+        //this is still 1
+        pid = fork();
+        if(pid < 0)
+        {
+            oops("Fork Failed!");
+        }
+        if (pid == 0)
+        {
+            //this is 1.2.2
+            if(execlp("./iam", "iam", "1.2.2", NULL) < 0)
+            oops("ForkFailed (1.2.2");
+        }
+        else
+        {
+            //this is still 1
+            if(execlp("./iam", "iam", "1", NULL) < 0)
+            oops("ForkFailed (1)");
+        }
+    }
+        //this is 1.2
+        if(execlp("./iam", "iam", "1.2", NULL) < 0)
+        oops("ForkFailed (1.2)");
+    }
 
     // pid > 0 ==> must be parent
-    printf("I am the parent %d\n", getpid());
+    printf("I am the parent, pid = %d\n", getpid());
     /* parent will wait for the child to complete */
     if (waitpid(-1, NULL, 0) < 0)
         printf("-1 from wait() with errno = %d\n", errno);
